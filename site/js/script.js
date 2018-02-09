bd4e0af1574583c5db1859859603f83ed662bf1b
Daniel <Daniel.kull@itggot.se>
2018-02-09 14:05:34 +0100

    Changed the style of the text, made small adjustments.
    Changed the font to Roboto.
    Adjusted poof image size.

--- site/js/script.js
+++ site/js/script.js
@@ -1,16 +1,50 @@
+var offers = false;
+var news = false;
+
 function ToggleSearch() {
     var element = document.querySelector(".searchDiv");
     element.classList.toggle("show");
 
     element = document.querySelector(".suggested");
     element.classList.toggle("showSuggested");
+
+    element = document.querySelector(".advSearch");
+    element.classList.toggle("advSearchPressed");
 }
 
-/*
-.menu {
-    display: none;
+function ExpandOffers() {
+    if(!offers) {
+        offers = true;
+        var element = document.querySelector(".suggested")
+        element.classList.add("offers");
+    }
 }
-.show {
-    display: block;
+
+function MinimizeOffers() {
+    if(offers) {
+        offers = false;
+        var element = document.querySelector(".suggested")
+        element.classList.remove("offers");
+    }
 }
-*/
+
+function OpenNews() {
+    if(!news) {
+        MinimizeOffers();
+        news = true;
+        var element = document.querySelector(".homepage");
+        element.classList.remove("show");
+        element = document.querySelector(".news");
+        element.classList.add("show");
+    }
+}
+
+function CloseNews () {
+    if(news) {
+        news = false;
+        var element = document.querySelector(".homepage");
+        element.classList.add("show");
+        element = document.querySelector(".news");
+        element.classList.remove("show");
+    }
+}
